ce88e2b3e8b46941a50fd0e23ec08d21fc28fec7
Shen Tian <biozinc@gmail.com>
2017-08-11 17:16:24 +0200

    MVP!!!

--- src/cljs/localhost3449/core.cljs
+++ src/cljs/localhost3449/core.cljs
@@ -7,17 +7,23 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Vars
 
-(defonce app-state
-  (reagent/atom {}))
+(defonce timer (reagent/atom (js/Date.)))
 
-
+(defonce time-updater (js/setInterval
+                       #(reset! timer (js/Date.)) 16))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Page
 
-(defn page [ratom]
+(defn page []
   [:div
-   "Welcome to reagent-figwheel."])
+   [:div (.toT)]
+   [:img {:src "/img/fig.png"
+          :width "50%"
+          :style {:transform (str "rotate(" 
+                                  (/ (.getTime @timer) 10)
+                                  "deg)")}}]
+   [:span "Maybe " [:code "lein figwheel"] " first?"]])
 
 
 
@@ -27,11 +33,10 @@
 (defn dev-setup []
   (when ^boolean js/goog.DEBUG
     (enable-console-print!)
-    (println "dev mode")
-    ))
+    (println "dev mode")))
 
 (defn reload []
-  (reagent/render [page app-state]
+  (reagent/render [page]
                   (.getElementById js/document "app")))
 
 (defn ^:export main []
