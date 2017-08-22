f140b3264c8e538c2de2f176a17df3b59f493dbf
Shen Tian <biozinc@gmail.com>
2017-08-22 21:09:04 +0200

    Vectorizing

--- src/cljs/localhost3449/core.cljs
+++ src/cljs/localhost3449/core.cljs
@@ -1,7 +1,6 @@
 (ns localhost3449.core
   (:require
-   [reagent.core :as reagent]
-   ))
+   [reagent.core :as reagent]))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -16,7 +15,7 @@
 ;; Page
 
 (defn angle! [rpm]
-  (mod (/ (.getTime @timer) rpm) 360))
+  (mod (* rpm (.getTime @timer) (/ 360 60000)) 360))
 
 (defn ring [count radius child]
   (into [:g] (for [theta (map #(* 360 (/ % count)) (range count))]
@@ -24,37 +23,36 @@
                                     "translate(" 0 " " radius ")")} 
                 child])))
 
+(defn rotate! [rpm]
+  {:transform (str "rotate(" (angle! (/ rpm 3)) " 0 0)")})
+
 (def pip 
   [:g
    [:circle {:cy 3 :r 5 :fill :orange}]
    [:circle {:r 3 :fill :white}]])
 
+
 (defn page []
   [:div.page
    [:div.fig
-    #_[:img {:src "/img/fig.png"
-           :style {:transform (str "rotate(" 
-                                   (/ (.getTime @timer) 10)
-                                   "deg)")}}]
-    [:svg {:height 200
-           :width 200}
+    [:svg {:height 300
+           :width 300
+           :viewBox "0 0 200 200"}
      [:g {:transform "translate(100,100)"}
-      [:circle {:cx 0 :cy 0 :r 100 
-                :fill :lightgreen
-                :stroke :purple
-                :stroke-width 3}]
-      [:circle {:cx 0 :cy 0 :r 75
+      [:circle {:r 100
+                :fill :purple}]
+      [:circle {:r 97 
+                :fill :lightgreen}]
+      [:circle {:r 90
                 :fill :fuchsia}]
       (into [:g] (for [{:keys [rpm n radius]} 
-                       [{:rpm 6  :n 8 :radius  10}
-                        {:rpm 8  :n 10 :radius 20}
-                        {:rpm 10 :n 12 :radius 30}
-                        {:rpm 12 :n 16 :radius 40}
-                        {:rpm 16 :n 20 :radius 50}
-                        {:rpm 20 :n 24 :radius 60}
-                        {:rpm 24 :n 28 :radius 70}
-                        {:rpm 28 :n 32 :radius 80}]]
-                   [:g {:transform (str "rotate(" (angle! rpm) " 0 0)")}
+                       [{:rpm 78 :n 8  :radius 13}
+                        {:rpm 65 :n 12 :radius 26}
+                        {:rpm 52 :n 18 :radius 39}
+                        {:rpm 39 :n 27 :radius 52}
+                        {:rpm 26 :n 40 :radius 65}
+                        {:rpm 13 :n 61 :radius 78}]]
+                   [:g (rotate! rpm)
                     [ring n radius pip]]))]]]
    [:div.text
     [:p "Maybe " [:code "lein figwheel"] " first?"]]])
